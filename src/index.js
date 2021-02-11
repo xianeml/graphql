de66e56e2fe1f62f7e844eeb7331abed3185d5c2
xianeml <xianemml@gmail.com>
2021-02-12 00:13:10 +0900

    connecting the server and db with prisma client

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'tutorial for GraphQL',
-  },
-];
+const prisma = new PrismaClient();
 
 /* GraphQL 스키마의 실제 구현
    각 필드와 동일한 이름을 가진 함수 구현
    모든 필드가 리졸버 함수를 가진다.
    리졸브: 쿼리를 처리하여 데이터 반환
 */
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => 'API입니다',
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-      links.push(link);
-      return link;
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
     },
   },
   /*
@@ -46,9 +42,13 @@ const fs = require('fs');
 const path = require('path');
 
 // 스키마와 리졸버를 서버에 전달
+// 서버가 초기화될 때 context도 초기화, context.prisma 접근 가능
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 // GraphQL playground 확인 가능
